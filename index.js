9212f10dfdf27e210300032205cd965be0e12d60
Shea Lignitz <rlignitz@gmail.com>
2017-11-10 23:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random();
 let randomInteger = Math.floor(randomNumber);
 let randomUserID = (Math.floor(randomNumber * 20) + 1) + 1000000000;
+/* if you want the number to be between 1000000005 and 1000000020, change it to
+(Math.floor(randomNumber * 16) + 5) + 1000000000; (randomNumber * 16 could only
+be a max of 15, and adding 5 to that gives 20)*/
